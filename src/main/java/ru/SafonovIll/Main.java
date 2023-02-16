c635fcc61860795ec5720abfe7d1c2b081144152
Ïlya Safonov <Safonov.ILL@yandex.ru>
2023-02-16 17:18:11 +0300

    add word

--- src/main/java/ru/SafonovIll/Main.java
+++ src/main/java/ru/SafonovIll/Main.java
@@ -2,6 +2,6 @@ package ru.SafonovIll;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Hello Hello Hello world!");
+        System.out.println("Hello Hello Hello hello Hello world!");
     }
 }
