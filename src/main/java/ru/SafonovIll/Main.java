2dcb4d62a54be2da1dbef4bf2b973ea43d244d51
Ïlya Safonov <Safonov.ILL@yandex.ru>
2023-03-10 14:41:48 +0300

    added hw for java1

--- src/main/java/ru/SafonovIll/Main.java
+++ src/main/java/ru/SafonovIll/Main.java
@@ -1,7 +1,43 @@
 package ru.SafonovIll;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("QA QA QA!");
+
+
+        int a = 25;
+        int b = 10;
+        int c = 33;
+        double d = 22.5;
+        boolean e = true;
+        boolean f = false;
+        int g = Integer.MAX_VALUE;
+
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a : b = " + (a / b));
+        System.out.println("a % b = " + (a % b));
+        System.out.println("c ++ = " + (c ++ ));
+        System.out.println("c -- = " + (c -- ));
+        System.out.println("-- c  = " + (-- c ));
+        System.out.println("++ c = " + (++ c ));
+
+
+        System.out.println("a == b " + (a == b) );
+        System.out.println("a != b " + (a != b) );
+        System.out.println("a > b " + (a > b) );
+        System.out.println("a < b " + (a < b) );
+
+
+        System.out.println("e && f = " + (e && f) );
+        System.out.println("e || f = " + (e || f) );
+        System.out.println("!(e && f) = " + !(e && f) );
+
+        System.out.println("int + double = c + d = " + (c + d));
+        System.out.println("int * double = c + d = " + (c * d));
+        System.out.println("Переполнение: " + (g + a ));
+
+
     }
 }
